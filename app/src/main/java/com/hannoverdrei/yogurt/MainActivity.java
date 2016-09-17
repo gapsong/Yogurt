c47552501174a98dbd0af6f852415d7bc540f4a7
gap <khiemtonthat@yahoo.de>
2016-09-17 03:29:44 +0200

    bitte kein git merge

--- app/src/main/java/com/hannoverdrei/yogurt/MainActivity.java
+++ app/src/main/java/com/hannoverdrei/yogurt/MainActivity.java
@@ -2,12 +2,30 @@ package com.hannoverdrei.yogurt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    ConnectionHandler connectionHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        connectionHandler = new ConnectionHandler();
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("yo", "fdsfa");
+                connectionHandler.sendGETstring();
+            }
+        });
+
     }
+
+
 }
