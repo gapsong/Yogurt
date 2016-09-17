be4116dd1deab0b9b264c31231743e8e1783c516
Tim Bernert <tim.bernert@gmx.de>
2016-09-17 10:09:59 +0200

    objects

---
+++ app/src/main/java/com/hannoverdrei/yogurt/Achievment.java
@@ -0,0 +1,16 @@
+package com.hannoverdrei.yogurt;
+
+/**
+ * Created by Tim on 17.09.2016.
+ */
+public class Achievment {
+    private int id;
+    private String name;
+    private int points;
+
+    public Achievment(int id, String name, int points) {
+        this.id = id;
+        this.name = name;
+        this.points = points;
+    }
+}
---
+++ app/src/main/java/com/hannoverdrei/yogurt/Voucher.java
@@ -0,0 +1,10 @@
+package com.hannoverdrei.yogurt;
+
+/**
+ * Created by Tim on 17.09.2016.
+ */
+public class Voucher {
+    private int id;
+    private String name;
+    private int percentage;
+}
